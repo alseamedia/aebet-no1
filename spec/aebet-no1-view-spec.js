3530c9a4a407832877f124273f2efe4d28450b6d
alseamedia <115203491+alseamedia@users.noreply.github.com>
2022-10-07 17:02:17 +0800

    Add files via upload

---
+++ spec/aebet-no1-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AebetNo1View from '../lib/aebet-no1-view';
+
+describe('AebetNo1View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
